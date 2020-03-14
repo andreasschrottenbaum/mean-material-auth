9bea9ff16cc6a59a43526c9f9af6565c178235cc
Andreas Schrottenbaum <andreas.schrottenbaum@gmail.com>
2020-03-14 17:33:35 +0100

    close navbar on route change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Router, Event, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +12,21 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild('drawer') drawer;
+
   constructor(
-    private breakpointObserver: BreakpointObserver
-  ) {}
+    private breakpointObserver: BreakpointObserver,
+    private router: Router
+  ) {
+    this.router.events.subscribe((event: Event) => {
+      // Close the navbar on route change on handset devices
+      if (event instanceof NavigationEnd) {
+        if (window.matchMedia(Breakpoints.Handset).matches) {
+          this.drawer.close();
+        }
+      }
+    });
+  }
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
