c4dc17773a3a8b1c7a65b567b9427073baea02bb
Andreas Schrottenbaum <andreas.schrottenbaum@gmail.com>
2020-03-14 16:03:23 +0100

    added DashboardComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from './modules/material.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
