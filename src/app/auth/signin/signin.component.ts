614c1228c4010d1b9b3f92fcf53f540e6aecbd5f
Andreas Schrottenbaum <andreas.schrottenbaum@gmail.com>
2020-03-21 03:35:58 +0100

    added login form

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { HeadlineService } from 'src/app/shared/services/headline.service';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private headline: HeadlineService
+  ) {
+    this.headline.setTitle('Login');
   }
 
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  save(data) {
+    console.log(data);
+  }
 }
